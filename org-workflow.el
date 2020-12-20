56dbb32d6f51f015fbd1c208392d2927eed1bdaf
Akira Komamura <akira.komamura@gmail.com>
2020-12-20 13:58:27 +0900

    Add package header

--- org-workflow.el
+++ org-workflow.el
@@ -1,3 +1,36 @@
+;;; org-workflow.el --- Org actions framework -*- lexical-binding: t -*-
+
+;; Copyright (C) 2020 Akira Komamura
+
+;; Author: Akira Komamura <akira.komamura@gmail.com>
+;; Version: 0.1
+;; Package-Requires: ((emacs "25.1") (dash "2.12"))
+;; Keywords: outlines convenience
+;; URL: https://github.com/akirak/org-workflow
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; FIXME
+
+;;; Code:
+
 (require 'dash)
 
 (defgroup org-workflow
@@ -170,3 +203,4 @@
   (call-interactively #'org-insert-last-stored-link))
 
 (provide 'org-workflow)
+;;; org-workflow.el ends here
